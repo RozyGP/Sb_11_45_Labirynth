f676f46c0708d8b30faa135b0f2e84f1ac24cace
RozyGP <rozmarynka19@gmail.com>
2022-04-23 13:21:38 +0200

    Door animation. Lock object and animation almost finished. Transition of animation to do.

---
+++ Assets/Lock.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lock : MonoBehaviour
+{
+    bool iCanOpen = false;
+
+    public Door door;
+    public KeyColor myKeyColor;
+    bool locked = false;
+    Animator key;
+
+    private void Start()
+    {
+        key = GetComponent<Animator>();
+    }
+
+    public void UseKey()
+    {
+        door.CloseOpen();
+    }
+
+    public bool CheckTheKey()
+    {
+        if(GameManager.gameManager.redKey > 0 && myKeyColor == KeyColor.Red)
+        {
+            GameManager.gameManager.redKey--;
+            locked = true;
+            return true;
+        } 
+        else if (GameManager.gameManager.greenKey > 0 && myKeyColor == KeyColor.Green)
+        {
+            GameManager.gameManager.greenKey--;
+            locked = true;
+            return true;
+        }
+        else if (GameManager.gameManager.goldKey > 0 && myKeyColor == KeyColor.Gold)
+        {
+            GameManager.gameManager.goldKey--;
+            locked = true;
+            return true;
+        }
+        else
+        {
+            Debug.Log("No key!");
+            return false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            iCanOpen = true;
+            Debug.Log("Can use lock");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            iCanOpen = false;
+            Debug.Log("Cannot use lock");
+        }
+    }
+}
